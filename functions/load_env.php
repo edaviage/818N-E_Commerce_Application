53332e50cb4b4c56ea129a9b1278b1f64c6a8c1d
Shankar Narayan <shankarnarayan20@gmail.com>
2024-10-17 16:03:32 -0400

    Added support for env file

---
+++ functions/load_env.php
@@ -0,0 +1,27 @@
+<?php
+
+function loadEnv($path)
+{
+    if (!file_exists($path)) {
+        throw new Exception('.env file not found');
+    }
+
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) {
+            // Skip comments
+            continue;
+        }
+
+        list($name, $value) = explode('=', $line, 2);
+        $name = trim($name);
+        $value = trim($value);
+
+        if (!array_key_exists($name, $_ENV)) {
+            putenv(sprintf('%s=%s', $name, $value));
+            $_ENV[$name] = $value;
+            $_SERVER[$name] = $value;
+        }
+    }
+}
--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,20 @@
 <?php 
-// $con=mysqli_connect('localhost','root','','ecommerce_1');
-$con = new mysqli('localhost','root','','ecommerce_1');
+
+use Dotenv\Dotenv;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$db_name = $_ENV['DB_NAME'];
+$port = $_ENV['DB_PORT'];
+
+$con = new mysqli($host, $username, $password, $db_name, $port);
+
 if(!$con){
     die(mysqli_error($con));
 }
