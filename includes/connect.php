4014b64f5e22c0ed15436ac54aeef1d8a3e1f9fc
Sagar Wagh <46933764+Sagarswagh@users.noreply.github.com>
2024-10-19 22:03:15 -0400

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,17 +1,17 @@
 <?php 
 // Database connection parameters
-$servername = 'ecommercedb.cf6me04ym26r.us-east-1.rds.amazonaws.com'; // Your RDS endpoint
-$username = 'admin'; // Your RDS master username
-$password = 'MyPassword123!'; // Your RDS master password
-$dbname = 'ecommerce_1'; // Your database name
+// $servername = 'ecommercedb.cf6me04ym26r.us-east-1.rds.amazonaws.com'; // Your RDS endpoint
+// $username = 'admin'; // Your RDS master username
+// $password = 'MyPassword123!'; // Your RDS master password
+// $dbname = 'ecommerce_1'; // Your database name
 
-// Create connection
-$con = new mysqli($servername, $username, $password, $dbname);
+// // Create connection
+// $con = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
-}
-echo "Connected successfully";
+// // Check connection
+// if ($con->connect_error) {
+//     die("Connection failed: " . $con->connect_error);
+// }
+// echo "Connected successfully";
 ?>
 
