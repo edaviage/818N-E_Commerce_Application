49e5d87a4c06bca9b40169581c01d6cda9b4e609
shareefm0 <shareefm@umd.edu>
2024-10-26 00:14:11 -0400

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../config.php';
+require_once __DIR__ . '/../config.php';
 
 // Create a new MySQLi connection using variables from config.php
 $con = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
