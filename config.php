744bb83361cf2f562b44f84e3a8f9f6c958b80af
Marc Gabriel N. Badilla <62585845+MarcBad98@users.noreply.github.com>
2024-10-28 18:10:57 -0400

    Revert "Created the config.php file"
    This reverts commit faeea02d301e25b094d1a5b48e9a295e6f6500a3.

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_SERVER', getenv('DB_SERVER'));
-define('DB_USERNAME', getenv('DB_USER'));
-define('DB_PASSWORD', getenv('DB_PASS'));
-define('DB_NAME', getenv('DB_NAME'));
-define('AWS_REGION', getenv('AWS_REGION'));
-define('S3_BUCKET', getenv('S3_BUCKET'));
-?>
